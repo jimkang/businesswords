19a29209c94b26b8218b1054f19c110ab890d2ed
Jim Kang <jimkang@gmail.com>
2018-01-26 20:37:56 -0500

    Post to archive.

--- post-business-tweet.js
+++ post-business-tweet.js
@@ -7,6 +7,24 @@ var async = require('async');
 var createWordnok = require('wordnok').createWordnok;
 var businessify = require('./businessify');
 
+var StaticWebArchiveOnGit = require('static-web-archive-on-git');
+var queue = require('d3-queue').queue;
+var randomId = require('idmaker').randomId;
+
+var staticWebStream = StaticWebArchiveOnGit({
+  config: config.github,
+  title: config.archiveName,
+  footerScript: `<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49491163-1', 'jimkang.com');
+  ga('send', 'pageview');
+</script>`,
+  maxEntriesPerPage: 50
+});
+
 var dryRun = false;
 if (process.argv.length > 2) {
   dryRun = process.argv[2].toLowerCase() == '--dry';
@@ -18,7 +36,7 @@ var wordnok = createWordnok({
 
 var twit = new Twit(config.twitter);
 
-async.waterfall([getTopics, pickFirst, businessify, postTweet], wrapUp);
+async.waterfall([getTopics, pickFirst, businessify, postToTargets], wrapUp);
 
 function getTopics(done) {
   var opts = {
@@ -38,16 +56,32 @@ function pickFirst(words, done) {
   }
 }
 
-function postTweet(text, done) {
+function postToTargets(text, done) {
   if (dryRun) {
     console.log('Would have tweeted:', text);
-    callNextTick(done);
   } else {
-    var body = {
-      status: text
-    };
-    twit.post('statuses/update', body, done);
+    var q = queue();
+    q.defer(postTweet, text);
+    q.defer(postToArchive, text);
+    q.await(done);
   }
+}
+
+function postTweet(text, done) {
+  var body = {
+    status: text
+  };
+  twit.post('statuses/update', body, done);
+}
+
+function postToArchive(text, done) {
+  var id = 'memo-' + randomId(8);
+  staticWebStream.write({
+    id,
+    date: new Date().toISOString(),
+    caption: text
+  });
+  staticWebStream.end(done);
 }
 
 function wrapUp(error) {
